5a74ec6e269b6480f3c6b689f1222a09020f299b
josh chatten <dw22682@bristol.ac.uk>
2023-01-27 12:28:58 +0000

    added the first function

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int first() {
+  for (int i = 0; i < 10; i++) {
+    printf("%i\n",i);
+  }
+  return 0;
+}
+
 int main() {
   puts("Hello World");
+  first();
   return 0;
 }
